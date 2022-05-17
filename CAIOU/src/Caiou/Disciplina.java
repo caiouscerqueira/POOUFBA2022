2d5c1f0df9df7782380986ee3069fdd696c7cd5d
caiou.cerqueira <caiou.cerqueira@CS2PC04.AD002.NET02>
2022-05-17 08:20:22 -0300

    Add github

---
+++ CAIOU/src/Caiou/Disciplina.java
@@ -0,0 +1,32 @@
+package Caiou;
+
+import java.util.Date;
+
+public class Disciplina {
+
+	public static void main(String[] args) {
+		Estudante c[] = new Estudante[3];
+		c[0] = new Estudante(1,"João Alberto","Medicina",0,0,0,"Inativo" );
+		c[1] = new Estudante(2, "Maria Santana", "Dança", 0, 0, 0,"Ativo");
+		c[2] = new Estudante(3,"Cassío Melo","Direito",0,0,0,"Ativo");
+		Protocolo p[] = new Protocolo[5];
+		p[0] = new Protocolo(1,"APROVEITAMENTO DE ESTUDOS ",new Date(),new Date(),false);
+		p[1] = new Protocolo(2,"TRANCAMENTO PARCIAL DE DISCIPLINAS",new Date(),new Date(),false);
+		p[2] = new Protocolo(3,"TRANCAMENTO TOTAL DE DISCIPLINAS ",new Date(),new Date(),false);
+		p[3] = new Protocolo(4,"ADICIONAR ATIVIDADES COMPLEMENTARES",new Date(),new Date(),false);
+		p[4] = new Protocolo(5,"MUDANÇA DE CURSO",new Date(),new Date(),false);
+		
+		
+		
+		c[1].abrir();
+		c[1].fechar();
+		c[0].abrir();
+		c[0].cancelar();
+		c[2].fechar();
+		c[2].cancelar();
+		c[1].status();
+		c[0].status();
+		c[2].status();
+	}
+
+}
---
+++ CAIOU/src/Caiou/Estudante.java
@@ -0,0 +1,83 @@
+package Caiou;
+
+public class Estudante {
+	//Atributos
+	private  int matricula;
+	private String nome;
+	private String curso;
+	private int abre;
+	private int fecha;
+	private int cancela;
+	private String status;
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public int getAbre() {
+		return abre;
+	}
+	public void setAbre(int abre) {
+		this.abre = abre;
+	}
+	public int getFecha() {
+		return fecha;
+	}
+	public void setFecha(int fecha) {
+		this.fecha = fecha;
+	}
+	public int getCancela() {
+		return cancela;
+	}
+	public void setCancela(int cancela) {
+		this.cancela = cancela;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	//Métodos públicos
+	public void abrir(){
+		this.setAbre(this.getAbre() + 1);
+	}	
+	public void fechar(){
+		this.setFecha(this.getFecha() + 1);
+	}
+	public void cancelar(){   
+		this.setCancela(this.getCancela() + 1);
+	}
+	
+	public void status(){   
+		System.out.println(" O estudante " + this.getNome() 
+		+" acompanhou mais um protocolo, agora possui: " + this.getAbre() + " aberto(s), " + this.getFecha() + " fechado(s), e " + this.getCancela() + " cancelado(s) "  );
+	}	
+	
+	public Estudante(int matricula, String nome, String curso, int abre, int fecha,
+			int cancela, String status) {
+		super();
+		matricula = matricula;
+		nome = nome;
+		curso = curso;
+		this.abre = abre;
+		this.fecha = fecha;
+		this.cancela = cancela;
+		this.status = status;
+	}
+	
+}
